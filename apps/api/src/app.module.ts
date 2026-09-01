5cfb13d6ff2c7079b2cdf6bdcb22c2e581e8ac51
ruturaj-testfyra <ruturaj@testfyra.com>
2026-09-01 18:23:17 +0530

    Fix: Correct import path from './app.fix' to './app.service'

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.fix';
-
+import { AppService } from './app.service';
 
 @Module({
   controllers: [AppController],
